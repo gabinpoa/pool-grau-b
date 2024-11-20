74ae3ceaf1c0cf81c74ef5ba99c35fdfcd30ff34
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2024-11-20 14:18:00 -0300

    lógica do execute de ComputeProcess

--- computingProcess.py
+++ computingProcess.py
@@ -5,18 +5,31 @@ Gabriel Jardim Nascimento
 from process import Process
 
 class ComputingProcess(Process):
+
+    _possible_operators = ['+', '-', '*', '/']
+
     def __init__(self, pid: int, expression: str):
         super().__init__(pid)
 
         # identifica o operador e guarda em self.operator
-        for operator in self.possible_operators:
+        for operator in self._possible_operators:
             if operator in expression:
-                self.operator = operator
+                self._operator = operator
                 break
 
         # separa os operandos e guarda em self.operands como inteiros
-        self.operands: list[int] = [int(operand) for operand in expression.split(self.operator)]
+        self._operands: list[int] = [int(operand) for operand in expression.split(self._operator)]
 
 
-    possible_operators = ['+', '-', '*', '/']
-
+    def execute(self):
+        match self._operator:
+            case '+':
+                print(self._operands[0] + self._operands[1])
+            case '-':
+                print(self._operands[0] - self._operands[1])
+            case '*':
+                print(self._operands[0] * self._operands[1])
+            case '/':
+                print(self._operands[0] / self._operands[1])
+            case _:
+                print('Operador inválido')
