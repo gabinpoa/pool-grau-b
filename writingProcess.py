140705ab137065dface03f659f9974c706780d03
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2024-11-21 05:10:14 -0300

    lógica do execute de WritingProcess

--- writingProcess.py
+++ writingProcess.py
@@ -7,6 +7,11 @@ from computingProcess import ComputingProcess
 
 class WritingProcess(Process):
     def __init__(self, pid: int, processes_pool: list[Process], expression: str):
+        """
+        processes_pool deve ser a fila original de processos do programa
+        expression é o calculo que será escrito no arquivo
+        Ex: '2+2'
+        """
         super().__init__(pid)
         self._processes_pool = processes_pool
         self._expression = expression
@@ -14,6 +19,6 @@ class WritingProcess(Process):
     def execute(self):
         with open('computation.txt', 'a') as f:
             f.writelines([self._expression])
-        pid = len(self._processes_pool)
+        pid = len(self._processes_pool) # Todo: Gerar um pid único
         self._processes_pool.append(ComputingProcess(pid, self._expression))
 
