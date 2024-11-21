8ae1b61a206f17609a00a5270ea9a124d58e9bac
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2024-11-21 16:03:48 -0300

    função main inicializando o programa

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 Arthur Martins Zimmermann
 Gabriel Jardim Nascimento
 """
+from menu import Menu
+from pool import Pool
 
 def main():
-    pass
+    pool = Pool()
+    menu = Menu(pool)
+    menu.run()
 
 if __name__ == "__main__":
     main()
