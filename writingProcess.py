0e0f11212a4809b3f2e188057778346123241670
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2024-11-21 05:29:44 -0300

    Corrigindo: WritingProcess não precisava criar um objeto de processo, apenas escrever no arquivo

--- writingProcess.py
+++ writingProcess.py
@@ -3,22 +3,16 @@ Arthur Martins Zimmermann
 Gabriel Jardim Nascimento
 """
 from process import Process
-from computingProcess import ComputingProcess
 
 class WritingProcess(Process):
-    def __init__(self, pid: int, processes_pool: list[Process], expression: str):
+    def __init__(self, pid: int, expression: str):
         """
-        processes_pool deve ser a fila original de processos do programa
         expression é o calculo que será escrito no arquivo
         Ex: '2+2'
         """
         super().__init__(pid)
-        self._processes_pool = processes_pool
         self._expression = expression
 
     def execute(self):
         with open('computation.txt', 'a') as f:
             f.writelines([self._expression])
-        pid = len(self._processes_pool) # Todo: Gerar um pid único
-        self._processes_pool.append(ComputingProcess(pid, self._expression))
-
